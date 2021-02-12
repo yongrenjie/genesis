40491fce8d3c0b55b8fa25a1eee4ae6d61770631
Jonathan Yong <yongrenjie@gmail.com>
2021-02-12 20:50:27 +0000

    scripts v2.0.1: only change FnMODE if user input is valid
    Calling XCMD("1 FnMODE $FM") leads to a TopSpin error "illegal
    enumeration type" if $FM is not a valid value for FnMODE.
    
    In particular, this can happen if the noah_xxxx AU programme is called
    via `xaup`, which (inexplicably) appends "xaup" to the argument list.
    Thus, when `xaup` was used to run noah_xxxx without any other arguments,
    argv[2] would be "xaup" which would then trigger the "illegal
    enumeration" error.
    
    We therefore skip setting FnMODE if argv[2] is "xaup", and on invalid
    input display a non-blocking dialog box to alert the user.

--- scripts/py/noah_nus.py
+++ scripts/py/noah_nus.py
@@ -5,11 +5,11 @@ Script to set up NUS for NOAH experiments. To turn on NUS, run `noah_nus` from
 the TopSpin command line. To disable NUS on a dataset where it was previously
 enabled, run `noah_nus off`.
 
-v: 2.0.0
+v: 2.0.1
 Jonathan Yong & Tim Claridge, University of Oxford
 Eriks Kupce, Bruker UK
 modified from original Python script by Maksim Mayzel, Bruker BioSpin AG
-7 Nov 2020
+12 February 2021
 """
 
 from __future__ import division, with_statement
--- version.mjs
+++ version.mjs
@@ -1,2 +1,4 @@
+// This is the version for the website only, not for scripts!
+
 const version = "2.0.0";
 export default version;
