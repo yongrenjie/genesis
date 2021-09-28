04e07b0454299f655c7bc050579a1d83e31cf86e
Jonathan Yong <yongrenjie@gmail.com>
2021-09-28 15:12:25 +0100

    scripts: more informative NUS error messages

--- scripts/py/noah_nus2.py
+++ scripts/py/noah_nus2.py
@@ -21,43 +21,43 @@ from de.bruker.nmr.mfw.root.UtilPath import getTopspinHome
 from de.bruker.nmr.prsc.dbxml.ParfileLocator import getParfileDirs
 
 
+NUS_ALREADY_ON = ("NUS has already been enabled for this dataset.\n"
+                  "Please disable NUS first with 'noah_nus2 off' before"
+                  " reconfiguring NUS.")
+NUS_ALREADY_OFF = ("NUS has not been enabled for this dataset yet.\n"
+                   "You can enable it using the command 'noah_nus2 on'.")
+MODULE_UNSUPPORTED = ("One or more modules in this supersequence (likely\nQF"
+                      " COSY, 2D J, or PSYCHE) are not compatible with NUS.")
+GENESIS_URL = "https://nmr-genesis.co.uk"
+PP_NOT_GENESIS = ("This pulse programme is not suitable for use\n"
+                  " with this NUS script. You may be able to use\n"
+                  " the old 'noah_nus.py' script; or you can download\n"
+                  " a new version of this pulse programme from the\n"
+                  " GENESIS website ({}).").format(GENESIS_URL)
+
+
 def enable_nus():
     # Check if NUS is already enabled.
     zgoptns, vclist = GETPAR("ZGOPTNS"), GETPAR("VCLIST")
     if "-DNUS" in zgoptns and "noah" in vclist:
-        ERRMSG("noah_nus2.py: NUS already enabled for this dataset.\n"
-               "Please disable NUS first with 'noah_nus2 off' before"
-               " reconfiguring NUS.")
-        EXIT()
+        err_exit(NUS_ALREADY_ON)
 
     # Check if the pulse programme supports -DNUS flag
     pulprog = GETPAR("PULPROG")
     for directory in getParfileDirs(0):  # gets pulprog directories
         found_pulprog = False
         if pulprog in os.listdir(directory):
-            found_pulprog = True
-            # first, check if it has the NUS flag
-            has_nus_flag = False
-            with open(os.path.join(directory, pulprog), "r") as file:
-                for line in file:
-                    if "#ifdef NUS" in line:
-                        has_nus_flag = True
-                        break
-            if not has_nus_flag:
-                ERRMSG("noah_nus2.py: This pulse programme does not support"
-                       " the -DNUS acquisition flag. Please re-download the"
-                       " pulse programme from the GENESIS website (https://"
-                       "nmr-genesis.co.uk), and make sure that this NOAH"
-                       " experiment does not contain any modules that are"
-                       " incompatible with NUS (e.g. QF COSY, 2DJ, or PSYCHE)."
-                       )
-                EXIT()
-            break
+            pulprog_fname = os.path.join(directory, pulprog)
+            if os.path.isfile(pulprog_fname):
+                found_pulprog = True
+                if not found_in_file(pulprog_fname, GENESIS_URL):
+                    err_exit(PP_NOT_GENESIS)
+                if not found_in_file(pulprog_fname, "#ifdef NUS"):
+                    err_exit(MODULE_UNSUPPORTED)
+                break
     # Exit if pulse programme was not found
     if not found_pulprog:
-        ERRMSG("noah_nus2.py: Pulse programme '{}'"
-               " was not found.".format(pulprog))
-        EXIT()
+        err_exit("Pulse programme '{}' was not found.".format(pulprog))
 
     # Reset FnTYPE.
     PUTPAR("FnTYPE", "traditional(planes)")
@@ -100,8 +100,7 @@ def enable_nus():
     exitcode = os.system(nusstr)
     # Check for errors.
     if exitcode != 0:
-        ERRMSG("noah_nus2.py: nussampler exited with code {}".format(exitcode))
-        EXIT()
+        err_exit("nussampler exited with code {}".format(exitcode))
 
     # Put the newly generated 'noah' list as the VCLIST parameter.
     PUTPAR("VCLIST", "noah")
@@ -117,9 +116,7 @@ def disable_nus():
     # Check if NUS is already enabled.
     zgoptns, vclist = GETPAR("ZGOPTNS"), GETPAR("VCLIST")
     if not ("-DNUS" in zgoptns and "noah" in vclist):
-        ERRMSG("noah_nus2.py: NUS has not been set up for this dataset yet.\n"
-               "It can be enabled using 'noah_nus2 on'.")
-        EXIT()
+        err_exit(NUS_ALREADY_OFF)
     # To undo NUS is easier, since we don't need to generate the list.
     # Back-calculate TD1.
     nus_td1 = int(GETPAR("1 TD"))
@@ -139,6 +136,19 @@ def disable_nus():
     # Remove vclist. See note above about using " " instead of "".
     PUTPAR("VCLIST", " ")
     EXIT()
+
+
+def err_exit(message):
+    ERRMSG(message, title="noah_nus2.py")
+    EXIT()
+
+
+def found_in_file(fname, needle):
+    with open(fname, "r") as file:
+        for line in file:
+            if needle in line:
+                return True
+    return False
 
 
 if __name__ == "__main__":
