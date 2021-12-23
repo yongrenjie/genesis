02c197ecfc7c5e495e280905cd38bd3f16d91986
Jonathan Yong <yongrenjie@gmail.com>
2021-12-23 23:54:47 +0000

    version: bump to 2.0.20

--- scripts/py/noah_nus2.py
+++ scripts/py/noah_nus2.py
@@ -5,11 +5,11 @@ Script to set up NUS for NOAH experiments. To turn on NUS, run `noah_nus2` from
 the TopSpin command line. To disable NUS on a dataset where it was previously
 enabled, run `noah_nus2 off`.
 
-v: 2.0.19
+v: 2.0.20
 Jonathan Yong & Tim Claridge, University of Oxford
 Eriks Kupce, Bruker UK
 modified from original Python script by Maksim Mayzel, Bruker BioSpin AG
-17 December 2021
+23 December 2021
 """
 
 from __future__ import division, with_statement
