3c918e90316fb933936c1308982b506120b8514a
Jonathan Yong <yongrenjie@gmail.com>
2021-12-28 16:26:57 +0000

    pprogs: make 15N HMBC CTP grads longer (p17)

--- src/modules/N_HMBC_CF.ts
+++ src/modules/N_HMBC_CF.ts
@@ -10,6 +10,7 @@ let preamble = `
 "d4     = 0.25s/cnst2"                ; 13C INEPT
 "d20    = 3u"                         ; 15N HSQC t1/2
 "in20   = 1000000/(2*cnst40*sfo3)"    ; 15N HSQC increment: cnst40 = SW(15N)
+"p17    = p16*cnst16"                 ; Longer gradients for 15N module
 "D[ID]a = (0.5s/cnst23)-p16-d16-4u"
 "D[ID]b = p16+d16+p2/2+d20-p21*2/PI+4u"
 "cnst44 = 2*sfo3/sfo1"                ; gradient ratio
@@ -32,20 +33,20 @@ let pulprog = `
   D[ID]b
   (p22 ph0):f3
   4u
-  p16:gp1
+  p17:gp1
   d16
   d20
   (p2 ph11):f1
   d20
   4u
-  p16:gp1
+  p17:gp1
   d16
   (p22 ph0):f3
   D[ID]b
   (p21 ph4):f3
   (p2 ph0):f1
   4u
-  p16:gp1*EA*G[ID]
+  p17:gp1*EA*G[ID]
   d16
   4u
   goscnp ph29
