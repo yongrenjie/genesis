85c74f6bfaa602c6ad9c865623f2bb9701b01680
Jonathan Yong <yongrenjie@gmail.com>
2022-07-23 02:11:54 +0100

    pprogs: fix t1 incrementation in N_HMBC_CQFDD

--- src/modules/N_HMBC_CFQDD.ts
+++ src/modules/N_HMBC_CFQDD.ts
@@ -1,14 +1,14 @@
 import { AF_NLP3 } from "../acquFlag.js";
 import NOAHModule from "../noahModule.js";
 
-let shortDescription = `; interleaved 15N HMBC (QF) with two different nJ(NH)`;
+let shortDescription = `; interleaved 15N HMBC (QF) with two different nJ(NH)`
 
 let preamble = `
 "p2     = p1*2"                       ; 1H hard 180
 "p22    = p21*2"                      ; 15N hard 180
 "d4     = 0.25s/cnst2"                ; 13C INEPT
-"d25    = 3u"                         ; 15N QF HMBC t1/2
-"in25   = 1000000/(2*cnst40*sfo3)"    ; 15N QF HMBC increment: cnst40 = SW(15N)
+"d20    = 3u"                         ; 15N interleaved QF HMBC t1/2
+"in20   = 1000000/(2*cnst40*sfo3)"    ; 15N interleaved QF HMBC increment: cnst40 = SW(15N)
 "p17    = p16*cnst16"                 ; Longer gradients for 15N module
 "D[ID]a = (0.5s/cnst23)-p16-d16-4u"   ; HMBC evolution 1
 "D[ID]b = (0.5s/cnst24)-p16-d16-4u"   ; HMBC evolution 2
@@ -32,11 +32,11 @@ else {
 }
   ; coherence transfer to 13C and t1
   (p21 ph12):f3
-  d25
+  d20
   p16:gp25
   d16
   (p2 ph11):f1
-  d25
+  d20
   p16:gp26
   d16
   (p21 ph0):f3
