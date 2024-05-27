7ac5695dd0cd753d9d4fe338f3ebe1c7d14c75e9
Jonathan Yong <yongrenjie@gmail.com>
2024-05-27 23:26:37 +0100

    version: bump to 2.3.2

--- scripts/py/noah_nus2.py
+++ scripts/py/noah_nus2.py
@@ -10,7 +10,7 @@ In practice, there is no good reason to retain the old version as long as you
 make sure to only use pulse programmes obtained from the GENESIS website:
 https://nmr-genesis.co.uk
 
-v: 2.3.1
+v: 2.3.2
 Jonathan Yong & Tim Claridge, University of Oxford
 Eriks Kupce, Bruker UK
 modified from original Python script by Maksim Mayzel, Bruker BioSpin AG
